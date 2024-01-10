d3d45802a7921f1c2ace86bd592544e4c3ad053b
Daniel Cunha <danielcunha@bemol.com.br>
2024-01-10 19:46:25 -0400

    desafio implementado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia("123456","Modelo 6120", "1111111111", 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone("5585236","14 Pro Max", "5555555555", 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
